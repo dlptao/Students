fa387a6e67c04fdf4d1cac545ee5790f84f0dc2c
MSI\taodan <taodan284@gmail.com>
2025-04-30 19:33:37 +0700

    (sửa lại từ file trước) client server fast api áp dụng clean architecture, solid.

--- student_management/backend/app/core/config.py
+++ student_management/backend/app/core/config.py
@@ -4,11 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = int(os.getenv("DB_PORT"))
+DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SQLALCHEMY_DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
--- student_management/backend/app/main.py
+++ student_management/backend/app/main.py
@@ -3,4 +3,8 @@ from app.routers import auth
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "Student Management API with SQLAlchemy is running!"}
+
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
